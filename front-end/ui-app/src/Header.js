8d6b10f484ab78bcba139f3081d0639a26741a31
unknown <nicolatesan96@gmail.com>
2022-06-25 17:09:06 +0200

    Add header and button for initialize game

---
+++ front-end/ui-app/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header id='header'>
+            <div className='container-title-header'>
+                <h1 className='title-header'>Benvenuto dentro quiz game</h1>
+                <ButtonStart/>
+            </div>
+        </header>
+    )
+}
+
+const ButtonStart = () => {
+  return (
+    <button className='button-reveal'>START GAME</button>
+  )
+}
+    
+export default Header
--- front-end/ui-app/src/Navbar.js
+++ front-end/ui-app/src/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const mouse = document.querySelector('.cursor');
 
-    // Mouse Track
+// Mouse Track
 window.addEventListener('mousemove', (e) => {
     mouse.style.top = e.pageY + 'px';
     mouse.style.left = e.pageX + 'px';
@@ -19,52 +19,26 @@ window.addEventListener('mouseover', (e) => {
     }
 })
 
-
 const Navbar = () => {
     return (
         <nav id='nav'>
             {/* Mouse Track */}
-        <div className="cursor"></div>
-                
-                {/* Icon top pag */}
-                <div className="reveal">
-                    <i className="fa-solid fa-arrow-turn-up fa-2x"></i>
+            <div className="cursor"></div>
+            
+            {/* Inizio Navbar */}
+            
+            <div id="navbar">
+                <div className="box-link">
+                    <a className="override-color" href="/">Quiz</a>
                 </div>
                 
-                {/* Inizio Navbar */}
-                
-                <nav id="navbar">
-                    <div className="box-link">
-                        <a className="override-color" href="/">Quiz</a>
-                    </div>
-                    
-                    <div className="btn-custom">
-                        <span className="nav-top"></span>
-                        <span className="nav-middle"></span>
-                        <span className="nav-middle-angle"></span>
-                        <span className="nav-bottom"></span>
-                    </div>
-                    
-                </nav>
-        
-                {/* Link della navbar a comparsa */}
-                <div className="link-utils">
-                    <a className="link-line-1" href="/test">
-                        <h2><span className="underline-custom-1"></span>chi siamo</h2>
-                    </a>
-                    <a className="link-line-2" href="#">
-                        <h2><span className="underline-custom-2"></span>servizi</h2>
-                    </a>
-                    <a className="link-line-3" href="#">
-                        <h2><span className="underline-custom-3"></span>portfolio</h2>
-                    </a>
-                    <a className="link-line-4" href="#">
-                        <h2><span className="underline-custom-4"></span>lavora con noi</h2>
-                    </a>
-                    <a className="link-line-5" href="#">
-                        <h2><span className="underline-custom-5"></span>contatti</h2>
-                    </a>
-                </div>
+                <div className="btn-custom">
+                    <span className="nav-top"></span>
+                    <span className="nav-middle"></span>
+                    <span className="nav-middle-angle"></span>
+                    <span className="nav-bottom"></span>
+                </div>    
+            </div>
         </nav>
     )
 }
