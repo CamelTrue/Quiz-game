922ab2d39c4778e9ed926a982f3465fb51435780
Dev1 - Tesan <tesiu1234@gmail.com>
2022-06-26 13:44:04 +0200

    Add aos in header

--- front-end/ui-app/src/Header.js
+++ front-end/ui-app/src/Header.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Header = () => {
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+
     return (
         <header id='header'>
             <div className='container-title-header'>
-                <h1 className='title-header'>Benvenuto dentro quiz game</h1>
+                <h1 className='title-header' data-aos="fade-left">Benvenuto dentro quiz game</h1>
                 <ButtonStart/>
             </div>
         </header>
@@ -13,7 +20,7 @@ const Header = () => {
 
 const ButtonStart = () => {
     return (
-        <button className='button-reveal'>START GAME</button>
+        <button className='button-reveal' data-aos="fade-left">START GAME</button>
     )
 }
     
