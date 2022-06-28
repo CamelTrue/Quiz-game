24c435711615f158f8dd3fa747afea1440dd0a4a
Dev1 - Tesan <tesiu1234@gmail.com>
2022-06-28 16:13:37 +0200

    test resolve for cors

--- front-end/ui-app/src/App.js
+++ front-end/ui-app/src/App.js
@@ -7,9 +7,9 @@ function App() {
 
     return (
         <div className="App">
-            <Header/>
+            {/* <Header/> */}
             <DataFetching />
-            <Login />
+            {/* <Login /> */}
         </div>
     );
 }
--- front-end/ui-app/src/DataFetching.js
+++ front-end/ui-app/src/DataFetching.js
@@ -3,53 +3,34 @@ import axios from 'axios'
 
 export default function DataFetching() {
 
-    // const [users, setUsers] = useState([])
-
-    // useEffect(() => {
-    //     fetchData()
-    // }, [])
-
-    // const fetchData = async () => {
-    //     fetch('http://127.0.0.1:8000/user')
-    //         .then(res => {
-    //             console.log(res.data);
-    //             const data = res.data;
-    //             return setUsers(data);
-    //         })
-    //     }
-
-    //     console.log(users)
-
-    // return (
-    //     <div>
-    //         {users.map((user) => {
-    //             return <p key={user.id_user}>{user.username}</p>
-    //         })}
-    //     </div>
-    // );
-
-    const [data, setData] = useState('')
-
-    const fetchApi = async () => {
-        await axios.get("127.0.0.1:8000/user")
-            .then((response) => {
-                const api = response.data // response object
-                return setData(api)
-            })
-    }
-
-    console.log(data)
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
-        fetchApi() // actually go fetch!
+        fetchData()
     }, [])
-    
-        // JSON.stringify() make a printable object (string) out of anything
+
+    const fetchData = async () => {
+        axios.get('http://127.0.0.1:8000/user')
+            .then((res) => {
+                console.log(res)
+                res.writeHead(200, {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                })
+                
+                const data = res.json()
+                return setUsers(data)
+            })
+        }
+
     return (
         <div>
             {
-                JSON.stringify(data)
-            } 
+                users.map((user) => {
+                   <p key={user.id_user}>{user.username}</p>
+                })
+            }
+            {users}
         </div>
-    )
+    );
 }
