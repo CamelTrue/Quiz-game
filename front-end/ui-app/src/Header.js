692a2692af6627982df079b8810667d59f106d9a
Dev1 - Tesan <tesiu1234@gmail.com>
2022-06-30 01:03:42 +0200

    Added commit for source code

--- front-end/ui-app/src/Header.js
+++ front-end/ui-app/src/Header.js
@@ -1,11 +1,17 @@
 import InterfaceQuestion from './InterfaceQuestion';
+
+// Importazione degli hooks
 import React, {useEffect, useState} from 'react'
+
+// Libreria per la creazione di animazioni
 import {motion} from 'framer-motion'
+
 import "aos/dist/aos.css";
 import AOS from "aos";
 import ErrorValidate from './ErrorValidate';
 import user_data from './user_data.json'
 
+// Libreria non utilizzata
 const Header = () => {
     useEffect(() => {
         AOS.init();
@@ -15,6 +21,8 @@ const Header = () => {
     return (
         <header id='header'>
             <div className='container-title-header'>
+
+                {/* motion serve per indicare che subirà l'animazione, animate con chiave x e valore 100 mi permette di spostare l'elemento sull'asse delle x */}
                 <motion.h1 className='title-header' animate={{x : 100}}>Benvenuto dentro quiz game</motion.h1>
                 <div className='container'>
                     <div className='row flex-row align-items-center'>
@@ -31,9 +39,12 @@ const Header = () => {
 
 const ButtonStart = () => {
     const [reveal, setReveal] = useState(false)
+
+    // Hook per tenere traccia dello stato dell'input
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    // Riferimento all'oggetto json user_data
     const user = [
         user_data[0].username,
         user_data[0].email,
@@ -47,13 +58,17 @@ const ButtonStart = () => {
     return (
         <div>
             <div className='form-input'>
+                {/* Con onChange teniamo traccia delle modifiche fatto all'input, quali? quelle del suo valore, ovvero dove scrivo ( ringrazia target ) */}
                 <motion.input id='email' value={email} onChange={(e) => setEmail(e.target.value)} animate={{x : 170}} placeholder='Inserisci la email' />
                 <motion.input id='password' value={password} onChange={(e) => setPassword(e.target.value)} animate={{x : 170}} placeholder='Inserisci la password' />
             </div>
+
+            {/* onClick chiama la funzione handle che mi aggiornerà lo stato di reveal che ora e' true */}
             <motion.button className='button-reveal' onClick={handleClick} animate={{x : 86}}>START GAME</motion.button>
             {reveal && (
                 <React.Fragment>
                     {
+                        // Thernary operator per la visualizzazione dell'interfaccia delle domande, altrimenti spunterà un errore
                         user[1] === email && user[2] === password ? <InterfaceQuestion /> : <ErrorValidate />
                     }
                 </React.Fragment>
--- front-end/ui-app/src/InterfaceQuestion.js
+++ front-end/ui-app/src/InterfaceQuestion.js
@@ -5,24 +5,29 @@ import {motion} from 'framer-motion'
 const InterfaceQuestion = () => {
 
     let arr = []
+
+    // Assegno alla chiave il riferimento della domanda
     const matcher = {
-        one : question[1].answer[0].first_question,
-        two : question[1].answer[1].second_question,
-        three : question[1].answer[2].third_question,
-        four : question[1].answer[3].fourth_question,
-        five : question[1].answer[4].fifth_question,
-        six : question[1].answer[5].sixth_question,
-        seven : question[1].answer[6].seventh_question,
-        eight : question[1].answer[7].eighth_question,
-        nine : question[1].answer[8].ninth_question,
-        ten : question[1].answer[9].tenth_question,
+        one : question[1].answer[0].first_answer,
+        two : question[1].answer[1].second_answer,
+        three : question[1].answer[2].third_answer,
+        four : question[1].answer[3].fourth_answer,
+        five : question[1].answer[4].fifth_answer,
+        six : question[1].answer[5].sixth_answer,
+        seven : question[1].answer[6].seventh_answer,
+        eight : question[1].answer[7].eighth_answer,
+        nine : question[1].answer[8].ninth_answer,
+        ten : question[1].answer[9].tenth_answer,
     }
 
+    // Hook per tenere traccia dello stato di count ( sarà essenziale )
     const [count, setCount] = useState(0)
 
     switch (count) {
         case 0:
             arr = []
+            // Mi faccio ritornare tutti i valori delle domande tramite Object.values
+            // Con map vado a mappare ricorsivamente tutti i dati che arriveranno dal mio json che ho racchiuso dentro matcher.one per il primo gruppo di risposte 
             Object.values(matcher.one).map((value) => {
                 arr.push(value)
                 if (arr.length === 5) {
@@ -120,10 +125,13 @@ const InterfaceQuestion = () => {
     return (
         <motion.section id='section-iq' 
             animate={{ x : 100, scale : 1 }}
+
+            // Con initial definisco la posizione di partenza del mio elemento allo start dell'animazione
             initial={{ scale : 0 }}
         >
             <div className='title-section-iq'>{count < 10 ? question[0].question[count] : <p>Domande finite</p>}</div>
             {
+                // easter egg
                 useEffect(() => {
                     const timer = setTimeout(() => {
                         console.log('Trovato 1, perchè non provi a trovare anche 2 e 3')
